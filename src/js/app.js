e479672828d2979078516868a9090ca9da4a804e
jaasu <hernandezalan007@gmail.com>
2024-09-11 18:06:04 -0600

    se modifica el header para tener un menu de hamburguesa en dispositivos móviles

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    eventListeners();
+});
+
+function eventListeners(){
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click')
+};
