c921d399449abcdac6cf28907e8f27f6a0b61f3e
jaasu <hernandezalan007@gmail.com>
2024-09-17 22:21:44 -0600

    se integra el header como componente en todas las páginas

--- base.php
+++ base.php
@@ -6,19 +6,6 @@
         <h1>Título Página</h1>
     </main>
 
-    <footer class="footer seccion">
-        <div class="contenedor-footer contenedor">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados 2024</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+    <?php
+    include './includes/templates/footer.php';
+?>
--- entrada.php
+++ entrada.php
@@ -26,19 +26,6 @@
         </div>
     </main>
 
-    <footer class="footer seccion">
-        <div class="contenedor-footer contenedor">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados 2024</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+    include './includes/templates/footer.php';
+?>
