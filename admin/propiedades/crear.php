cc64899e6d5c21749e5fc74c96091496e6710f94
jaasu <hernandezalan007@gmail.com>
2024-09-19 17:35:47 -0600

    se almacenan los datos ingresados al formulario usando el método POST

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,33 @@
 <?php
+
+    //Base de datos
+    require '../../includes/config/database.php';
+    $db = conectarDB();
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedorId = $_POST['vendedor'];
+
+        //INSERTAR EN LA BASE DE DATOS
+
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId')";
+
+        echo $query;
+
+        
+    }
+
+    
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -7,6 +36,48 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Título</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Título Propiedad">
+
+                <label for="precio">Precio</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción</label>
+                <textarea id="descripcion" name="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones</label>
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños</label>
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedor" id="">
+                    <option value="1">Alan</option>
+                    <option value="2">Angie</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
     </main>
 
 <?php
