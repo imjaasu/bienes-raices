f55d4d46e750d338091ea359d64157c1ad7ab509
jaasu <hernandezalan007@gmail.com>
2024-09-23 20:25:57 -0600

    se añade una tabla donde se muestran las propiedades y sus detalles

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,31 @@
         <h1>Administrador de Bienes Raíces</h1>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="/imagenes/09c318c2f974be0c4f060df7f74f92b1.jpg" alt="" class="imagen-tabla"></td>
+                    <td>$1200000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
