6fd1bfc5b1be138e404b1e503a76d0b986325edd
jaasu <hernandezalan007@gmail.com>
2024-09-27 12:47:20 -0600

    se creó una función para verificar si un usuario está autenticado

--- includes/funciones.php
+++ includes/funciones.php
@@ -3,5 +3,18 @@
 require 'app.php';
 
 function incluirTemplate(string $nombre, bool $inicio = false){
-    include TEMPLATES_URL . "/${nombre}.php";
+    include TEMPLATES_URL . "/$nombre.php";
+}
+
+function estaAutenticado() : bool{
+
+    session_start();
+    
+    $autenticado = $_SESSION['login'];
+
+    if(!$autenticado){
+        return true;
+    } 
+
+    return false;
 }
